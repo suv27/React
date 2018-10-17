6371baf96e407f95b9d9d715c57829516a1e5fcb
physsi07 <starlinu33@gmail.com>
2018-10-16 22:57:13 -0400

    set up od the project as a clean one to start working on the movie-finder

---
+++ movie-showtime-finder/src/Component/App/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
