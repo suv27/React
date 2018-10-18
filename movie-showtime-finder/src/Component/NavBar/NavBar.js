8fca1d7015e14575af76421e199c62f6b13a8024
physsi07 <starlinu33@gmail.com>
2018-10-18 18:44:29 -0400

    mininful changes

--- movie-showtime-finder/src/Component/NavBar/NavBar.js
+++ movie-showtime-finder/src/Component/NavBar/NavBar.js
@@ -6,11 +6,10 @@ const Wrapper = styled.div`
   display: flex
   flex-row: row nowrap
   justify-content: flex-start
-
 `
 const LineWrap = styled.div `
   width: 35px
-  height: 30px
+  height: 35px
   cursor: pointer
   margin: 15px 0 0 15px
   display: flex
@@ -22,50 +21,21 @@ const Lines = styled.div `
   width: 35px
   height: 2px
   background-color: #FFF
-  display: none
-`
-const Line1 = styled.div`
-  width: 35px
-  height: 2px
-  // display: none
-  background-color: #FFF
-  position: relative
-  top: 7px
-  transform: rotate(45deg)
-`
-const Line2 = styled.div`
-  width: 35px
-  height: 2px
-  // display: none
-  background-color: #FFF
-  position: relative
-  bottom: 7px
-  left: -1px
-  transform: rotate(-45deg)
 `
 
 export default class Nav extends React.Component {
 
-  // constructor(props) {
-  //   super(props);
-  //
-  // }
-
-  opened = () => {
-    console.log("CLICKED");
-    
+  constructor(props) {
+    super(props);
   }
-
 
   render(){
     return(
       <Wrapper>
-        <LineWrap onClick={this.opened}>
+        <LineWrap >
           <Lines />
           <Lines />
           <Lines />
-          <Line1 />
-          <Line2 />
         </LineWrap>
       </Wrapper>
     )
