95059a8ccd885186fcf03b6fbbc165d2ca77cb65
physsi07 <starlinu33@gmail.com>
2018-10-18 01:25:17 -0400

    adding the close button for the entire button, missing the logic that will toggle from close to open and open to close

--- movie-showtime-finder/src/Component/NavBar/NavBar.js
+++ movie-showtime-finder/src/Component/NavBar/NavBar.js
@@ -22,6 +22,25 @@ const Lines = styled.div `
   width: 35px
   height: 2px
   background-color: #FFF
+  display: none
+`
+const Line1 = styled.div`
+  width: 35px
+  height: 2px
+  // display: none
+  background-color: #FFF
+  position: relative
+  top: 7px
+  transform: rotate(45deg)
+`
+const Line2 = styled.div`
+  width: 35px
+  height: 2px
+  // display: none
+  background-color: #FFF
+  position: relative
+  bottom: 7px
+  transform: rotate(-45deg)
 `
 
 export default class Nav extends React.Component {
@@ -33,6 +52,7 @@ export default class Nav extends React.Component {
 
   opened = () => {
     console.log("CLICKED");
+    
   }
 
 
@@ -43,6 +63,8 @@ export default class Nav extends React.Component {
           <Lines />
           <Lines />
           <Lines />
+          <Line1 />
+          <Line2 />
         </LineWrap>
       </Wrapper>
     )
