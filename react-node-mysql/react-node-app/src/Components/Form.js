233df338971f21af6d23033be254aca82199603e
physsi07 <starlinu33@gmail.com>
2018-12-07 01:40:52 -0500

    trying to map through all user to be able to display them on the screen

--- react-node-mysql/react-node-app/src/Components/Form.js
+++ react-node-mysql/react-node-app/src/Components/Form.js
@@ -5,62 +5,35 @@ export default class Form extends Component{
   constructor(props){
     super(props)
     this.state = {
-      users: {
-        username: '',
-        passcode: '',
-      }
+      user: [],
     }
   }
 
   componentDidMount(){
-
-    this.getUser();
-
-  }
-
-  getUser = () => {
-    fetch('http://localhost:4000/users', {
-        method: "GET", // *GET, POST, PUT, DELETE, etc.
-        mode: "no-cors", // no-cors, cors, *same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit
-        headers: {
-            "Content-Type": "application/json; charset=utf-8",
-            // "Content-Type": "application/x-www-form-urlencoded",
-        },
-        redirect: "follow", // manual, *follow, error
-        referrer: "no-referrer", // no-referrer, *client
-    }).then((x)=> console.log(x)).catch((err)=>console.log(err));
-  }
-  addUser = () => {
-    // const { users } = this.state;
-    // fetch(`http://localhost:4000/login/add=username${users.username}&passcode=${users.passcode}`)
+    fetch('http://localhost:4000/users')
+    .then(response => { response.json() })
+    .then(users => this.setState({ users }))
   }
 
   render(){
+
+    let { users } = this.state;
+
     return(
       <div>
-        <form>
-          <div>
-            <label>Username:</label>
-            <input
-              id="username"
-              type="text"
-              placeholder="Username" />
-          </div>
-          <div>
-            <label>Password:</label>
-            <input
-              id="password"
-              type="password"
-              placeholder="Password" />
-          </div>
-          <div>
-            <button
-              type="submit"
-              onClick={this.getUser}>Submit</button>
-          </div>
-        </form>
+        <table>
+          <thead>
+            <tr>
+              <th>Usernames</th> |
+              <th>Passcodes</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((username, passcode) =>
+              <tr><td>{username}</td><td>{passcode}</td></tr>
+            )}
+          </tbody>
+        </table>
       </div>
     )
   }
